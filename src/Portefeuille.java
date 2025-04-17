14ca6ccedd72b8db5dae2d8370a9662e0a0a5ae7
ordi2129194 <ordi2129194@educ.normandie.fr>
2025-04-17 15:45:13 +0200

    Message

--- src/Portefeuille.java
+++ src/Portefeuille.java
@@ -19,9 +19,12 @@ public class Portefeuille {
    * @return Vrai si la transaction a été effectuée, faux sinon.  
    */
   public boolean transfertDevise (Portefeuille destination, double montantJetons){
-      /**
-           FONCTION À IMPLEMENTER
-	  **/
+      if (this.montant >= destination.getMontant() && this.proprietaire.equals(destination.getProprietaire()))
+      {
+         this.montant -= montantJetons;
+         destination.montant += montantJetons;
+         return true;
+      }
       return false;
   }
 
@@ -33,9 +36,11 @@ public class Portefeuille {
    * @return true si le montant en euros est supérieur ou égal à 0 
    */
   public boolean achatDevise (double montantEuros){
-	/**
-           FONCTION À IMPLEMENTER
-	**/
+	if (montantEuros >= 0)
+    {
+        this.montant += montantEuros / this.monnaie.getValeurDeJeton();
+        return true;
+    }
     return false;
   }
 
